976d48d31d024f716aa89d2faae002f798bb22b3
samar <samsrivastva@gmail.com>
2023-08-28 10:10:59 +0530

    Add button added

---
+++ 01-gui-simple-bonus.py
@@ -0,0 +1,17 @@
+import PySimpleGUI as sg
+
+label1 = sg.Text('Select file to compress')
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse('Choose')
+
+label2 = sg.Text('Select destination folder')
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse('Choose')
+
+compress_button = sg.Button("Compress")
+
+window = sg.Window('My Todo App',layout=[[label1, input1, choose_button1],
+                                         [label2, input2, choose_button2],
+                                         [compress_button]])
+window.read() # code is halted at this place
+window.close()
--- gui.py
+++ gui.py
@@ -2,9 +2,24 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text('Type in a to do')
-input_box = sg.InputText(tooltip='Enter Todo')
+input_box = sg.InputText(tooltip='Enter Todo',key='todo')
 add_button = sg.Button("Add")
-window = sg.Window('My Todo App',layout=[[label, input_box],[add_button]])
+window = sg.Window('My Todo App',
+                   layout=[[label, input_box],
+                           [add_button]],
+                   font=('Helvetica','12'))
 
-window.read() # code is halted at this place
+# event = window.read() # code is halted at this place
+while True:
+    event, values = window.read()
+    print(event,values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
