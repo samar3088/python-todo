8ff332970a1a632f9aa3cc5c12feea2a59d82dc5
samar <samsrivastva@gmail.com>
2023-08-31 07:54:23 +0530

    Edit function added

--- gui.py
+++ gui.py
@@ -4,9 +4,18 @@ import PySimpleGUI as sg
 label = sg.Text('Type in a to do')
 input_box = sg.InputText(tooltip='Enter Todo',key='todo')
 add_button = sg.Button("Add")
+
+list_box = sg.Listbox(values=functions.get_todos(),
+                      key='todos',
+                      enable_events=True,
+                      size=(45,18))
+
+edit_button = sg.Button("Edit")
+
 window = sg.Window('My Todo App',
                    layout=[[label, input_box],
-                           [add_button]],
+                           [add_button],
+                           [list_box, edit_button]],
                    font=('Helvetica','12'))
 
 # event = window.read() # code is halted at this place
@@ -19,6 +28,17 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
